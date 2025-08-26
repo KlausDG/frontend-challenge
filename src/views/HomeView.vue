1edf379d6a2053f3862bf63e2d7b6389ca6bb1b6
Klaus <klausdgalm@hotmail.com>
2025-08-26 16:37:23 -0300

    feat: consume the api data and apply proxy to bypass CORS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { inject, onMounted } from 'vue'
+import type OrderGateway from '@/gateway/OrderGateway'
+
+const orderGateway = inject('orderGateway') as OrderGateway
+
+onMounted(async () => {
+  const data = await orderGateway.getOrderById(1)
+  console.log(data)
+})
 </script>
 
 <template>
